04b4d021f74f7d1314df6b53ca910fb564570289
kenmaready <ken@maready.com>
2023-05-21 14:19:27 -0400

    Add decreased look sensitivity when in Zoom mode

--- Assets/InputSystem/StarterAssetsInputs.cs
+++ Assets/InputSystem/StarterAssetsInputs.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46e382a5e470d1e2a5ccfa43b580b2a05e57daded9ed720d384bc042dbc0cc4b
-size 1870
+oid sha256:a28ff091b5b9a085f7743e8fefead418b68023e582091f4d144323cf5909f670
+size 1926
--- Assets/Player/Weapon/WeaponZoom.cs
+++ Assets/Player/Weapon/WeaponZoom.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf154f0f071392ee9821230e3d6780957e10b0b0d54b08a9982647273a7d0dec
-size 1290
+oid sha256:c3e940c59430d724f2e94b603afda1d754f0037f52c79ffbcfc3aef15c007342
+size 1439
