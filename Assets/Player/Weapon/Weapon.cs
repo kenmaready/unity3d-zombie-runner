012ca771bf4405fcc640e64dda1a61a933c4f099
kenmaready <ken@maready.com>
2023-05-21 21:55:30 -0400

    Add basic ammo clip concept to weapon

--- Assets/Player/Weapon/Weapon.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c68730f423a3e9f9fef10477900d5f8df71e8d8dcaa0344ec547165b035d7844
-size 1517
---
+++ Assets/Player/Weapons/Weapon.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:06b9efa3c1203abb0f7f4a6cca681309feb06812718e9e75d6dd42bd2fc1e67f
+size 1758
